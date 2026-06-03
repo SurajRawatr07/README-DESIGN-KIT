9f0bdbe1abcb974e459f772d3e056c3bf1e91798
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:00:09 +0530

    Update treeParser.ts

--- src/utils/treeParser.ts
+++ src/utils/treeParser.ts
@@ -2,10 +2,4 @@
 
 export const 
     const treeCharsRegex = /^([│├─└\s]*)(.*)$/;
-    const match = line.match(tre
-    const icon = isFolder
-      ? getIconForFolder(trimmedContent.replace('/', ''))
-      : getIconForFile(trimmedContent);
-
-    // 5. Reconstruct the line
-    // Don't add ico
+    co
