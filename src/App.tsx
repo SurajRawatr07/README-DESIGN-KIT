c321c8f565d1a15d76f24432175227b3b4db2d15
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:02:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import obile(window.innerWidth < 768);
+ile(window.innerWidth < 768);
     };
     che
