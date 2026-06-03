a65b11fde93db71dcc5879a7b69d5b91b59d4cfc
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:00:03 +0530

    Update treeParser.ts

--- src/utils/treeParser.ts
+++ src/utils/treeParser.ts
@@ -8,12 +8,4 @@ export const
       : getIconForFile(trimmedContent);
 
     // 5. Reconstruct the line
-    // Don't add icon if one already exists
-    const hasIcon = /[\u{1F300}-\u{1F9FF}]/u.test(trimmedContent);
-    const finalContent = hasIcon ? trimmedContent : `${icon} ${trimmedContent}`;
-
-    return `${treePrefix}${finalContent}`;
-  });
-
-  return `\`\`\`bash\n${processedLines.join('\n')}\n\`\`\``;
-};
+    // Don't add ico
