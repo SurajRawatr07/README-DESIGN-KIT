5e0f350a9d7ea60cb450a643d2b68997e3c43cd4
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:02:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import obile(window.innerWidth < 768);
     };
-    checkMobile();
-    windoScrollRestoration />
-              <Routes>
-
-                <Route path="/" element={<Layout><Home /></Layout>} />
-                <Route path="/elements" element={<Layout><Elements /></Layout>} />
-                <Route path="/templates" element={<Layout><TemplateLibraryPage /></Layout>} />
-                <Route path="/projects" element={<Layout><ProjectsSection /></Layout>} />
-                <Route path="/submit" element={<Layout><SubmitSection /></Layout>} />
-                <Route path="/drag-drop" element={<Layout><DragDropEditor /></Layout>} />
-                
-
-                <Route path="/about" element={<Layout><AboutUs /></Layout>} />
-                <Route path="/privacy" element={<Layout><PrivacyPolicy /></Layout>} />
+    checkMobile();privacy" element={<Layout><PrivacyPolicy /></Layout>} />
                 <Route path="/terms" element={<Layout><TermsOfService /></Layout>} />
                 <Route path="/readme-generator" element={<Layout><ReadmeGenerator /></Layout>} />
                 <Route path="/readme-editor" element={<ReadmeEditor />} />
