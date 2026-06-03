1f3f358f5b6500b6b9a4e114f54858fb644ce219
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:12:58 +0530

    Update dom-to-image-more.d.ts

--- src/types/dom-to-image-more.d.ts
+++ src/types/dom-to-image-more.d.ts
@@ -2,6 +2,4 @@ declare module 'dom-to-image-more' {
   export interface Options {
     filter?: (node: Node) => boolean;
     bgcolor?: string;
-    width?: numb\ber;
-    imagePlaceholder?: string;
- toPng: (node: Nod Node, options?: Options) => Promise<Uint8ClampedArray>;
+    wid
