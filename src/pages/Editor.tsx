4cfe0c980bb1bb3f51a79953b8833176a4d6fff1
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:38:25 +0530

    Update Editor.tsx

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -7,12 +7,7 @@ export const EditorPage = () => {
   const [variables, setVariables] = useState({ NAME: '', GITHUB_USER: '' });
   // 1. Fixes initialElements erroreChange={handleVariableChange} />
       
-      <main className="flex-1 overflow-auto p-8">
-        <div className="max-w-4xl mx-auto space-y-6">
-          <h1 className="text-xl font-bold">README Preview</h1>
-          {/* 4. Use ReadmePreview to render the processed elements */}
-          <ReadmePreview elements={dynamicElements} preset='default' onPresetChange={()=>{}}/>
-        </div>
+      <main clas
       </main>
     </div>
   );
