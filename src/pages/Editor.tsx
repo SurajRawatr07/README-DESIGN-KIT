d9433da27b4f9ffb485cfd762c707031fb8fc553
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-03 23:38:31 +0530

    Update Editor.tsx

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -3,12 +3,4 @@ import { TemplateUtils } from '@/utils/templateUtils';
 import { Sidebar } from '@/components/Editor/Sidebar'; 
 import { ReadmePreview } from '@/components/ReadmePreview'; 
 import type { E
-export const EditorPage = () => {
-  const [variables, setVariables] = useState({ NAME: '', GITHUB_USER: '' });
-  // 1. Fixes initialElements erroreChange={handleVariableChange} />
-      
-      <main clas
-      </main>
-    </div>
-  );
-};
+exp
